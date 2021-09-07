fcbaf24d664a06096cc896085196e029b24c3e84
VPuosk <vesa.puoskari@gmail.com>
2021-09-07 07:16:01 +0300

    Osa2 2.6

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+
+// yksittäisen numeron käsittelijä
+const Numero = ({ numero }) => {
+  return (
+    <div>
+      {numero.name}
+    </div>
+  )
+}
+
+// kaikkien numeroiden käsittelijä
+const Numerot = ({ numerot }) => {
+  return (
+    <div>
+      {numerot.map(numero => 
+        <Numero key={numero.name} numero={numero} />
+      )}
+    </div>
+  )
+}
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  // mikä käsittelee 'onSubmit' tapahtumaa
+  const tapahtumankäsittelijä = (tapahtuma) => {
+    tapahtuma.preventDefault()
+    // console.log('Jottai tapahtusi', tapahtuma.target)
+    // tehdään uusi henkilö/numero 'olio'
+    const personObj = {
+      name : newName,
+    }
+    setPersons(persons.concat(personObj))
+  }
+
+  // mikä käsittelee 'onChange' muutosta
+  const muutoksenkäsittelijä = (muutos) => {
+    //console.log(tapahtuma.target.value)
+    setNewName(muutos.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={tapahtumankäsittelijä}>
+        <div>
+          name: <input
+                  value = {newName}
+                  onChange = {muutoksenkäsittelijä}
+                />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Numerot numerot={persons} />
+    </div>
+  )
+
+}
+
+export default App
