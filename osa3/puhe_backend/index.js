15be3e1f27eca907329220f96040fd69c17f43ce
VPuosk <vesa.puoskari@gmail.com>
2021-09-09 19:39:57 +0300

    osa3 - static osion lisäys

--- osa3/puhe_backend/index.js
+++ osa3/puhe_backend/index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(function (tokens, req, res) {
   if (tokens.method(req,res) == "POST") {
     const data = JSON.stringify(req.body)
