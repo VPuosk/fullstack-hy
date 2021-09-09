f38c0f1b6d5cb6b27fbb99b60b40f6441e82cae4
VPuosk <vesa.puoskari@gmail.com>
2021-09-09 18:00:32 +0300

    Osa3 3.9 backend

--- osa3/puhe_backend/index.js
+++ osa3/puhe_backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
   if (tokens.method(req,res) == "POST") {
@@ -128,6 +130,6 @@ app.delete('/api/persons/:id', (pyynto, vastaus) => {
   vastaus.status(204).end()
 })
 
-const port = 3001
+const port = process.env.PORT || 3001
 app.listen(port)
 console.log(`Server running on port ${port}`)
