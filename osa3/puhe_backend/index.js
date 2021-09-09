1ff366fcbad337267eca48efc578526089fe4257
VPuosk <vesa.puoskari@gmail.com>
2021-09-09 12:38:00 +0300

    Osa3 3.8

--- osa3/puhe_backend/index.js
+++ osa3/puhe_backend/index.js
@@ -3,7 +3,26 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(function (tokens, req, res) {
+  if (tokens.method(req,res) == "POST") {
+    const data = JSON.stringify(req.body)
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      data
+    ].join(' ')
+  }
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+}))
 
 let persons = [
   {
