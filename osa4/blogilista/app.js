728aa2f6eec884192615950f365d1010fba9087e
VPuosk <vesa.puoskari@gmail.com>
2021-09-12 12:12:22 +0300

    Osa4 4.2 moduulijako + materiaalista lisää

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,32 @@
+const config = require('./utils/config')
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+
+const mongoUrl = config.MONGODB_URI
+
+logger.info('connecting to MONGODB')
+
+mongoose.connect(mongoUrl)
+  .then(() => {
+    logger.info('connected')
+  })
+  .catch((error) => {
+    logger.error('not connected', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
