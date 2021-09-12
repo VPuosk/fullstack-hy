bc6002ecf30bde8862b6e092917dc189df3ed12f
VPuosk <vesa.puoskari@gmail.com>
2021-09-12 13:15:29 +0300

    Osa4 4.4 lisää testejä

--- osa4/blogilista/tests/list.test.js
+++ osa4/blogilista/tests/list.test.js
@@ -6,3 +6,60 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('totalLikes', () => {
+  test('test many', () =>  {
+    const blogs = [
+      {
+        _id: 'AAAAAABBBBB',
+        title: 'Testi 1',
+        author: 'DEVES',
+        url: 'dev/null',
+        likes: 15,
+        __v: 0
+      },
+      {
+        _id: 'BBBBBCCCCC',
+        title: 'Testi 1',
+        author: 'DEVES',
+        url: 'dev/null',
+        likes: 5,
+        __v: 0
+      },
+      {
+        _id: 'CCCCCDDDDD',
+        title: 'Testi 3',
+        author: 'DEVES',
+        url: 'dev/null',
+        likes: 25,
+        __v: 0
+      },
+    ]
+
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(15 + 5 + 25)
+  })
+
+  test('test one', () =>  {
+    const blogs = [
+      {
+        _id: 'AAAAAABBBBB',
+        title: 'Testi 1',
+        author: 'DEVES',
+        url: 'dev/null',
+        likes: 15,
+        __v: 0
+      },
+    ]
+
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(15)
+  })
+
+  test('none', () =>  {
+    const blogs = []
+
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(0)
+  })
+})
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -4,6 +4,20 @@ const dummy = (blogs) => {
   return 1 + nBlogs - nBlogs
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+
+  const likes = blogs.map(blog => blog.likes)
+
+  const reducer = (sum, item) => {
+    return sum + item
+  }
+  return likes.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
