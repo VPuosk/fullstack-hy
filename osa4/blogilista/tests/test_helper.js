8059c74c3e32926a3bdb42417c220c3b1647b7fd
VPuosk <vesa.puoskari@gmail.com>
2021-09-14 07:41:45 +0300

    Osa4 4.15 & 4.16

--- osa4/blogilista/tests/test_helper.js
+++ osa4/blogilista/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const defaultBlogs = [
   {
@@ -32,7 +33,13 @@ const blogsInDB = async () => {
   return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
 module.exports = {
   defaultBlogs,
   blogsInDB,
+  usersInDb,
 }
---
+++ osa4/blogilista/tests/user_api.test.js
@@ -0,0 +1,41 @@
+//const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+const User = require('../models/blog')
+
+describe('User API tests', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation test', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('`username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })
+})
