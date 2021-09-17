d99c3176cdff9cacece6fb0d26ceae2e5a7b6dff
VPuosk <vesa.puoskari@gmail.com>
2021-09-17 16:15:03 +0300

    Osa5 5.17

---
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function() {
+    cy.visit('http://localhost:3000')
+    cy.contains('Login')
+  })
+})
+/*
+describe('Blog ', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Teppo Testaaja',
+      username: 'ttappi',
+      password: 'topsekret'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
+    cy.visit('http://localhost:3000')
+  })
+
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:3000')
+    cy.contains('Login')
+  })
+})
+*/
