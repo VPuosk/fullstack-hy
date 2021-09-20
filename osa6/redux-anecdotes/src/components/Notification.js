3f8305c4aadc9d6bcfe8be1446414a38c09a58d6
VPuosk <vesa.puoskari@gmail.com>
2021-09-20 17:41:11 +0300

    Osa6 6.19

--- osa6/redux-anecdotes/src/components/Notification.js
+++ osa6/redux-anecdotes/src/components/Notification.js
@@ -1,23 +1,38 @@
 
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notifications)
-  console.log(notification)
+const Notification = (props) => {
+
+  const anecdoteToShow = () => {
+    if (props.notifications === null) {
+      return null
+    }
+
+    return (
+      <div style={style}>
+        {props.notifications.content}
+      </div>
+    )
+  }
+
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
-  if (notification == null) {
-    return null
-  }
+
   return (
-    <div style={style}>
-      {notification.content}
-    </div>
+    anecdoteToShow()
   )
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notifications: state.notifications
+  }
+}
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+
+export default ConnectedNotification
