549b9beed5d7520808a5871a34d6b8685a756fd6
VPuosk <vesa.puoskari@gmail.com>
2021-09-30 18:19:42 +0300

    Osa8 - 3

--- osa8/library_backend/index.js
+++ osa8/library_backend/index.js
@@ -91,10 +91,18 @@ const typeDefs = gql`
     genres: [String]!
   }
 
+  type Author {
+    name: String!
+    id: String!
+    born: Int
+    bookCount: Int
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -103,6 +111,12 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) => {
+      return books.filter(book => book.author === root.name).length
+    }
   }
 }
 
