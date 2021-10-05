18da890963355689a1a7779c7439b7fd5bed555f
VPuosk <vesa.puoskari@gmail.com>
2021-10-05 14:20:41 +0300

    Osa9 7

--- osa9/typescript/src/exerciseCalculator.ts
+++ osa9/typescript/src/exerciseCalculator.ts
@@ -1,54 +1,9 @@
-interface exerciseData {
-  periodLength: number,
-  trainingDays: number,
-  success: boolean,
-  rating: number,
-  ratingDescription: string,
-  target: number,
-  average: number;
-}
+import { calculateExercises } from './exerciseCalc';
 
 interface exerciseDataInput {
   target: number,
   days: number[]
 }
-
-const calculateExercises = ( inputArray: number[], inputTarget: number):exerciseData => {
-  //const inputArray = [1];
-  //const inputTarget = 1;
-  const data: exerciseData = {
-    periodLength: 0,
-    trainingDays: 0,
-    success: false,
-    rating: 0,
-    ratingDescription: 'n/a',
-    target: 0,
-    average: 0
-  };
-  
-  data.periodLength = inputArray.length;
-
-  if (data.periodLength > 0) {
-    data.trainingDays = inputArray.filter(hours => hours > 0).length;
-    data.target = inputTarget;
-    data.average = inputArray.reduce((sum, value) => sum + value, 0)/data.periodLength;
-    data.success = data.periodLength === inputArray.filter(hours => hours >= data.target).length;
-    if (data.success) {
-      data.rating = 3;
-      data.ratingDescription = 'Excellent';
-    } else if (data.trainingDays < 2) {
-      data.rating = 1;
-      data.ratingDescription = 'Could be improved';
-    } else {
-      data.rating = 2;
-      data.ratingDescription = 'Good';
-    }
-  } else {
-    data.target = inputTarget;
-  }
-
-  return data;
-};
 
 const parseValues = (args:string[]):exerciseDataInput => {
   if (args.length < 4) {
--- osa9/typescript/src/index.ts
+++ osa9/typescript/src/index.ts
@@ -1,7 +1,16 @@
 import express from 'express';
 import { calculateBmi } from './bmiCalc';
+import { calculateExercises } from './exerciseCalc';
+import { Request } from 'express';
 //const express = require('express');
 const app = express();
+
+app.use(express.json());
+
+interface ExerciseRequest {
+  daily_exercises: number[],
+  target: number
+}
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -32,6 +41,33 @@ app.get('/bmi', (req, res) => {
     height:valueA,
     bmi:result
   });
+});
+
+// fair bit of work & stackoverflow browsing to get the lint to go silent...
+app.post('/exercises', (req: Request<Record<string, unknown>, unknown, ExerciseRequest>, res) => {
+  const { daily_exercises, target } = req.body;
+
+  // validating the existance of parameters
+  if (!daily_exercises || !target) {
+    res.status(400).send({
+      error: "parameters missing"
+    });
+    return;
+  }
+
+  const inputTarget = Number(target);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const inputArray:number[] = daily_exercises.map((a: any) => Number(a)); // deliberate use of any
+
+  // validating the type of parameters
+  if (!inputTarget || inputArray.some(isNaN)) {
+    res.status(400).send({
+      error: "malformatted parameters"
+    });
+    return;
+  }
+
+  res.send(calculateExercises(inputArray, inputTarget));
 });
 
 const PORT = 3002;
