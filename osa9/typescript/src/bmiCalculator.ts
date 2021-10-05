a89459ca5da0128a8785271d5879aa68186723e1
VPuosk <vesa.puoskari@gmail.com>
2021-10-05 08:53:04 +0300

    Osa9 2

--- osa9/typescript/src/bmiCalculator.ts
+++ osa9/typescript/src/bmiCalculator.ts
@@ -1,25 +1,25 @@
 const calculateBmi = (height:number, weight:number):string => {
   //console.log('h',height)
   //console.log('w',weight)
-  const bmi:number = 100 * 100 * weight / height / height
+  const bmi:number = 100 * 100 * weight / height / height;
   //console.log('bmi',bmi)
   if (bmi < 16) {
-    return 'Underweight (Severe thinness)'
+    return 'Underweight (Severe thinness)';
   } else if (bmi < 17) {
-    return 'Underweight (Moderate thinness)'
+    return 'Underweight (Moderate thinness)';
   } else if (bmi < 18.5) {
-    return 'Underweight (Mild thinness)'
+    return 'Underweight (Mild thinness)';
   } else if (bmi < 25) {
-    return 'Normal range'
+    return 'Normal range';
   } else if (bmi < 30) {
-    return 'Overweight (Pre-obese)'
+    return 'Overweight (Pre-obese)';
   } else if (bmi < 35) {
-    return 'Obese (Class I)'
+    return 'Obese (Class I)';
   } else if (bmi < 40) {
-    return 'Obese (Class II)'
+    return 'Obese (Class II)';
   } else {
-    return 'Obese (Class III)'
+    return 'Obese (Class III)';
   }
 }
 
-console.log(calculateBmi(180,74))
+console.log(calculateBmi(180,74));
---
+++ osa9/typescript/src/exerciseCalculator.ts
@@ -0,0 +1,48 @@
+interface exerciseData {
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  rating: number,
+  ratingDescription: string,
+  target: number,
+  average: number;
+};
+
+const calculateExercises = ( inputArray: number[], inputTarget: number):exerciseData => {
+  //const inputArray = [1];
+  //const inputTarget = 1;
+  const data: exerciseData = {
+    periodLength: 0,
+    trainingDays: 0,
+    success: false,
+    rating: 0,
+    ratingDescription: 'n/a',
+    target: 0,
+    average: 0
+  };
+  
+  data.periodLength = inputArray.length;
+
+  if (data.periodLength > 0) {
+    data.trainingDays = inputArray.filter(hours => hours > 0).length;
+    data.target = inputTarget;
+    data.average = inputArray.reduce((sum, value) => sum + value, 0)/data.periodLength;
+    data.success = data.periodLength === inputArray.filter(hours => hours >= data.target).length;
+    if (data.success) {
+      data.rating = 3;
+      data.ratingDescription = 'Excellent';
+    } else if (data.trainingDays < 2) {
+      data.rating = 1;
+      data.ratingDescription = 'Could be improved';
+    } else {
+      data.rating = 2;
+      data.ratingDescription = 'Good';
+    }
+  } else {
+    data.target = inputTarget;
+  }
+
+  return data;
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
