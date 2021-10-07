3c2ee0608ffeba4d1162cc236522a9f8a0d8a190
VPuosk <vesa.puoskari@gmail.com>
2021-10-07 14:34:31 +0300

    Osa9 20

--- osa9/patientor-frontend/src/components/PatientPage.tsx
+++ osa9/patientor-frontend/src/components/PatientPage.tsx
@@ -8,10 +8,24 @@ import { useParams } from 'react-router-dom';
 import { updatePatient } from "../state/reducer";
 
 const PatientPage = () => {
+  //const [ currentPatient, setCurrentPatient ] = useState<Patient>( );
   const [{ patients }, dispatch] = useStateValue();
   const { id } = useParams<{ id: string }>();
 
   const currentPatient = patients[id];
+
+  const fetchPatient = async () => {
+
+    if (currentPatient && currentPatient.ssn === undefined ) {
+      console.log(`fetching data for ${currentPatient.name}`);
+      const { data : patient } = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
+      dispatch(updatePatient(patient));
+    }
+  };
+
+  if (currentPatient && !(currentPatient.ssn)) {
+    void fetchPatient();
+  }
 
   if (!currentPatient) {
     return (
@@ -21,19 +35,40 @@ const PatientPage = () => {
     );
   }
 
-  const fetchPatient = async () => {
-    if (currentPatient.ssn === undefined ) {
-      const { data : patient } = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
-      dispatch(updatePatient(patient));
+  
+  //void fetchPatient();
+
+  const renderEntries = () => {
+    if (currentPatient.entries === undefined) {
+      return (
+        null
+      );
     }
+    return (
+      <div>
+        {currentPatient.entries.map(entry => (
+          <div key={entry.id}>
+            {entry.date} {entry.description}
+            <div>
+              {entry.diagnosisCodes?.map(diagcode => (
+                <div key={diagcode}>{diagcode}</div>
+              ))}
+            </div>
+          </div>
+        )
+        )}
+      </div>
+    );
   };
-  void fetchPatient();
+
 
   return (
     <div>
-      <div>{currentPatient.name}</div>
+      <h2>{currentPatient.name}</h2>
       <div>{currentPatient.ssn}</div>
       <div>{currentPatient.occupation}</div>
+      <h3>Entries</h3>
+      {renderEntries()}
     </div>
   );
 };
--- osa9/patientor-frontend/src/types.ts
+++ osa9/patientor-frontend/src/types.ts
@@ -11,9 +11,48 @@ export enum Gender {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+export type Entry = HealthCheckEntry | HospitalEntry | OccupationalHealthCareEntry;
+
+interface HospitalDischarge {
+  date: string;
+  criteria: string;
 }
 
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthCareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface HospitalEntry  extends BaseEntry {
+  type: "Hospital";
+  discharge: HospitalDischarge;
+}
 
 export interface Patient {
   id: string;
